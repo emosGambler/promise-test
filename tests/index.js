8c92e74f3cfc9f0ef602d293a9abc17b5717e091
Jacek Góraj <jckgrj@gmail.com>
2018-04-27 14:13:05 +0200

    WIP

--- tests/index.js
+++ tests/index.js
@@ -1,24 +1,42 @@
 const request = require('request');
 
 var promise1 = new Promise((resolve, reject) => {
-	resolve('1');
+	resolve('3');
 });
 var promise2 = new Promise((resolve, reject) => {
 	request('https://http.cat/200', function (error, response, body) {
-		console.log('2');
+		console.log('4');
 		resolve(response.statusCode);
 	});
 });
 var promise3 = new Promise((resolve, reject) => {
-	resolve('3');
+	resolve('5');
 });
 
-promise1.then(value => {
-	console.log(value);
-	return promise2;
-}).then(value => {
-	console.log(value);
-	return promise3;
-}).then(value => {
-	console.log(value);
+const scenario = (title, callback) => {
+	var promise4 = new Promise((resolve, reject) => {
+		resolve(title);
+	});
+	var promise5 = new Promise((resolve, reject) => {
+		callback();
+		resolve('2');
+	});
+	promise4.then(value => {
+		console.log(value);
+		return promise5;
+	}).then(value => {
+		console.log(value);
+	});
+};
+
+scenario('1', () => {
+	promise1.then(value => {
+		console.log(value);
+		return promise2;
+	}).then(value => {
+		console.log(value);
+		return promise3;
+	}).then(value => {
+		console.log(value);
+	});
 });
